7fe34e14ef377867c72d76580c714f14e8413d7e
kanchan76 <67153067+kanchan76@users.noreply.github.com>
2020-07-12 15:52:49 +0530

    Update Checkbox.py

--- Checkbox.py
+++ Checkbox.py
@@ -1,7 +1,7 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path=r"C:\Users\rajdeep.m\PycharmProjects\duringclass\resources/chromedriver.exe") # r for raw string in order to avoid /n
+driver = webdriver.Chrome(executable_path=r"../resources\ChromeDriver83/chromedriver.exe") # r for raw string in order to avoid /n
 driver.maximize_window()
 driver.get("http://cookbook.seleniumacademy.com/Config.html")
 driver.find_element_by_xpath("//input[@value='ABS']").click()
